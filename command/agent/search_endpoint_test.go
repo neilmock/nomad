c2de6e34b192e92471f93e2ca05c87744eee2004
Seth Hoenig <shoenig@hashicorp.com>
2021-03-14 19:13:21 -0500

    wip modernize prefix tests

--- command/agent/search_endpoint_test.go
+++ command/agent/search_endpoint_test.go
@@ -7,108 +7,111 @@ import (
 
 	"github.com/hashicorp/nomad/nomad/mock"
 	"github.com/hashicorp/nomad/nomad/structs"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestHTTP_SearchWithIllegalMethod(t *testing.T) {
-	assert := assert.New(t)
+func TestHTTP_PrefixSearchWithIllegalMethod(t *testing.T) {
 	t.Parallel()
+
 	httpTest(t, nil, func(s *TestAgent) {
 		req, err := http.NewRequest("DELETE", "/v1/search", nil)
-		assert.Nil(err)
+		require.NoError(t, err)
 		respW := httptest.NewRecorder()
 
 		_, err = s.Server.SearchRequest(respW, req)
-		assert.NotNil(err, "HTTP DELETE should not be accepted for this endpoint")
+		require.EqualError(t, err, "Invalid method")
+	})
+}
+
+func TestHTTP_FuzzySearchWithIllegalMethod(t *testing.T) {
+	t.Parallel()
+
+	httpTest(t, nil, func(s *TestAgent) {
+		req, err := http.NewRequest("DELETE", "/v1/search/fuzzy", nil)
+		require.NoError(t, err)
+		respW := httptest.NewRecorder()
+
+		_, err = s.Server.SearchRequest(respW, req)
+		require.EqualError(t, err, "Invalid method")
 	})
 }
 
 func createJobForTest(jobID string, s *TestAgent, t *testing.T) {
-	assert := assert.New(t)
-
 	job := mock.Job()
 	job.ID = jobID
 	job.TaskGroups[0].Count = 1
 
 	state := s.Agent.server.State()
 	err := state.UpsertJob(structs.MsgTypeTestSetup, 1000, job)
-	assert.Nil(err)
+	require.NoError(t, err)
 }
 
-func TestHTTP_Search_POST(t *testing.T) {
-	assert := assert.New(t)
+func TestHTTP_PrefixSearch_POST(t *testing.T) {
+	t.Parallel()
 
 	testJob := "aaaaaaaa-e8f7-fd38-c855-ab94ceb89706"
 	testJobPrefix := "aaaaaaaa-e8f7-fd38"
-	t.Parallel()
+
 	httpTest(t, nil, func(s *TestAgent) {
 		createJobForTest(testJob, s, t)
 
 		data := structs.SearchRequest{Prefix: testJobPrefix, Context: structs.Jobs}
 		req, err := http.NewRequest("POST", "/v1/search", encodeReq(data))
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		respW := httptest.NewRecorder()
 
 		resp, err := s.Server.SearchRequest(respW, req)
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		res := resp.(structs.SearchResponse)
-
-		assert.Equal(1, len(res.Matches))
+		require.Len(t, res.Matches, 1)
 
 		j := res.Matches[structs.Jobs]
-
-		assert.Equal(1, len(j))
-		assert.Equal(j[0], testJob)
-
-		assert.Equal(res.Truncations[structs.Jobs], false)
-		assert.NotEqual("0", respW.HeaderMap.Get("X-Nomad-Index"))
+		require.Len(t, j, 1)
+		require.Equal(t, testJob, j[0])
+		require.False(t, res.Truncations[structs.Jobs])
+		require.NotEqual(t, "0", respW.HeaderMap.Get("X-Nomad-Index"))
 	})
 }
 
-func TestHTTP_Search_PUT(t *testing.T) {
-	assert := assert.New(t)
+func TestHTTP_PrefixSearch_PUT(t *testing.T) {
+	t.Parallel()
 
 	testJob := "aaaaaaaa-e8f7-fd38-c855-ab94ceb89706"
 	testJobPrefix := "aaaaaaaa-e8f7-fd38"
-	t.Parallel()
+
 	httpTest(t, nil, func(s *TestAgent) {
 		createJobForTest(testJob, s, t)
 
 		data := structs.SearchRequest{Prefix: testJobPrefix, Context: structs.Jobs}
 		req, err := http.NewRequest("PUT", "/v1/search", encodeReq(data))
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		respW := httptest.NewRecorder()
 
 		resp, err := s.Server.SearchRequest(respW, req)
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		res := resp.(structs.SearchResponse)
-
-		assert.Equal(1, len(res.Matches))
+		require.Len(t, res.Matches, 1)
 
 		j := res.Matches[structs.Jobs]
-
-		assert.Equal(1, len(j))
-		assert.Equal(j[0], testJob)
-
-		assert.Equal(res.Truncations[structs.Jobs], false)
-		assert.NotEqual("0", respW.HeaderMap.Get("X-Nomad-Index"))
+		require.Len(t, j, 1)
+		require.Equal(t, testJob, j[0])
+		require.False(t, res.Truncations[structs.Jobs])
+		require.NotEqual(t, "0", respW.HeaderMap.Get("X-Nomad-Index"))
 	})
 }
 
-func TestHTTP_Search_MultipleJobs(t *testing.T) {
-	assert := assert.New(t)
+func TestHTTP_PrefixSearch_MultipleJobs(t *testing.T) {
+	t.Parallel()
 
 	testJobA := "aaaaaaaa-e8f7-fd38-c855-ab94ceb89706"
 	testJobB := "aaaaaaaa-e8f7-fd38-c855-ab94ceb89707"
 	testJobC := "bbbbbbbb-e8f7-fd38-c855-ab94ceb89707"
-
 	testJobPrefix := "aaaaaaaa-e8f7-fd38"
 
-	t.Parallel()
 	httpTest(t, nil, func(s *TestAgent) {
 		createJobForTest(testJobA, s, t)
 		createJobForTest(testJobB, s, t)
@@ -116,190 +119,175 @@ func TestHTTP_Search_MultipleJobs(t *testing.T) {
 
 		data := structs.SearchRequest{Prefix: testJobPrefix, Context: structs.Jobs}
 		req, err := http.NewRequest("POST", "/v1/search", encodeReq(data))
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		respW := httptest.NewRecorder()
 
 		resp, err := s.Server.SearchRequest(respW, req)
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		res := resp.(structs.SearchResponse)
-
-		assert.Equal(1, len(res.Matches))
+		require.Len(t, res.Matches, 1)
 
 		j := res.Matches[structs.Jobs]
+		require.Len(t, j, 2)
+		require.Contains(t, j, testJobA)
+		require.Contains(t, j, testJobB)
+		require.NotContains(t, j, testJobC)
 
-		assert.Equal(2, len(j))
-		assert.Contains(j, testJobA)
-		assert.Contains(j, testJobB)
-		assert.NotContains(j, testJobC)
-
-		assert.Equal(res.Truncations[structs.Jobs], false)
-		assert.NotEqual("0", respW.HeaderMap.Get("X-Nomad-Index"))
+		require.False(t, res.Truncations[structs.Jobs])
+		require.NotEqual(t, "0", respW.HeaderMap.Get("X-Nomad-Index"))
 	})
 }
 
-func TestHTTP_Search_Evaluation(t *testing.T) {
-	assert := assert.New(t)
-
+func TestHTTP_PrefixSearch_Evaluation(t *testing.T) {
 	t.Parallel()
+
 	httpTest(t, nil, func(s *TestAgent) {
 		state := s.Agent.server.State()
 		eval1 := mock.Eval()
 		eval2 := mock.Eval()
 		err := state.UpsertEvals(structs.MsgTypeTestSetup, 9000, []*structs.Evaluation{eval1, eval2})
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		prefix := eval1.ID[:len(eval1.ID)-2]
 		data := structs.SearchRequest{Prefix: prefix, Context: structs.Evals}
 		req, err := http.NewRequest("POST", "/v1/search", encodeReq(data))
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		respW := httptest.NewRecorder()
 
 		resp, err := s.Server.SearchRequest(respW, req)
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		res := resp.(structs.SearchResponse)
-
-		assert.Equal(1, len(res.Matches))
+		require.Len(t, res.Matches, 1)
 
 		j := res.Matches[structs.Evals]
-		assert.Equal(1, len(j))
-		assert.Contains(j, eval1.ID)
-		assert.NotContains(j, eval2.ID)
-
-		assert.Equal(res.Truncations[structs.Evals], false)
-		assert.Equal("9000", respW.HeaderMap.Get("X-Nomad-Index"))
+		require.Len(t, j, 1)
+		require.Contains(t, j, eval1.ID)
+		require.NotContains(t, j, eval2.ID)
+		require.False(t, res.Truncations[structs.Evals])
+		require.Equal(t, "9000", respW.HeaderMap.Get("X-Nomad-Index"))
 	})
 }
 
-func TestHTTP_Search_Allocations(t *testing.T) {
-	assert := assert.New(t)
-
+func TestHTTP_PrefixSearch_Allocations(t *testing.T) {
 	t.Parallel()
+
 	httpTest(t, nil, func(s *TestAgent) {
 		state := s.Agent.server.State()
 		alloc := mock.Alloc()
 		err := state.UpsertAllocs(structs.MsgTypeTestSetup, 7000, []*structs.Allocation{alloc})
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		prefix := alloc.ID[:len(alloc.ID)-2]
 		data := structs.SearchRequest{Prefix: prefix, Context: structs.Allocs}
 		req, err := http.NewRequest("POST", "/v1/search", encodeReq(data))
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		respW := httptest.NewRecorder()
 
 		resp, err := s.Server.SearchRequest(respW, req)
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		res := resp.(structs.SearchResponse)
-
-		assert.Equal(1, len(res.Matches))
+		require.Len(t, res.Matches, 1)
 
 		a := res.Matches[structs.Allocs]
-		assert.Equal(1, len(a))
-		assert.Contains(a, alloc.ID)
+		require.Len(t, a, 1)
+		require.Contains(t, a, alloc.ID)
 
-		assert.Equal(res.Truncations[structs.Allocs], false)
-		assert.Equal("7000", respW.HeaderMap.Get("X-Nomad-Index"))
+		require.False(t, res.Truncations[structs.Allocs])
+		require.Equal(t, "7000", respW.HeaderMap.Get("X-Nomad-Index"))
 	})
 }
 
-func TestHTTP_Search_Nodes(t *testing.T) {
-	assert := assert.New(t)
-
+func TestHTTP_PrefixSearch_Nodes(t *testing.T) {
 	t.Parallel()
+
 	httpTest(t, nil, func(s *TestAgent) {
 		state := s.Agent.server.State()
 		node := mock.Node()
 		err := state.UpsertNode(structs.MsgTypeTestSetup, 6000, node)
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		prefix := node.ID[:len(node.ID)-2]
 		data := structs.SearchRequest{Prefix: prefix, Context: structs.Nodes}
 		req, err := http.NewRequest("POST", "/v1/search", encodeReq(data))
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		respW := httptest.NewRecorder()
 
 		resp, err := s.Server.SearchRequest(respW, req)
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		res := resp.(structs.SearchResponse)
-
-		assert.Equal(1, len(res.Matches))
+		require.Len(t, res.Matches, 1)
 
 		n := res.Matches[structs.Nodes]
-		assert.Equal(1, len(n))
-		assert.Contains(n, node.ID)
+		require.Len(t, n, 1)
+		require.Contains(t, n, node.ID)
 
-		assert.Equal(res.Truncations[structs.Nodes], false)
-		assert.Equal("6000", respW.HeaderMap.Get("X-Nomad-Index"))
+		require.False(t, res.Truncations[structs.Nodes])
+		require.Equal(t, "6000", respW.HeaderMap.Get("X-Nomad-Index"))
 	})
 }
 
-func TestHTTP_Search_Deployments(t *testing.T) {
-	assert := assert.New(t)
-
+func TestHTTP_PrefixSearch_Deployments(t *testing.T) {
 	t.Parallel()
+
 	httpTest(t, nil, func(s *TestAgent) {
 		state := s.Agent.server.State()
 		deployment := mock.Deployment()
-		assert.Nil(state.UpsertDeployment(999, deployment), "UpsertDeployment")
+		require.NoError(t, state.UpsertDeployment(999, deployment), "UpsertDeployment")
 
 		prefix := deployment.ID[:len(deployment.ID)-2]
 		data := structs.SearchRequest{Prefix: prefix, Context: structs.Deployments}
 		req, err := http.NewRequest("POST", "/v1/search", encodeReq(data))
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		respW := httptest.NewRecorder()
 
 		resp, err := s.Server.SearchRequest(respW, req)
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		res := resp.(structs.SearchResponse)
-
-		assert.Equal(1, len(res.Matches))
+		require.Len(t, res.Matches, 1)
 
 		n := res.Matches[structs.Deployments]
-		assert.Equal(1, len(n))
-		assert.Contains(n, deployment.ID)
-
-		assert.Equal("999", respW.HeaderMap.Get("X-Nomad-Index"))
+		require.Len(t, n, 1)
+		require.Contains(t, n, deployment.ID)
+		require.Equal(t, "999", respW.HeaderMap.Get("X-Nomad-Index"))
 	})
 }
 
-func TestHTTP_Search_NoJob(t *testing.T) {
-	assert := assert.New(t)
-
+func TestHTTP_PrefixSearch_NoJob(t *testing.T) {
 	t.Parallel()
+
 	httpTest(t, nil, func(s *TestAgent) {
 		data := structs.SearchRequest{Prefix: "12345", Context: structs.Jobs}
 		req, err := http.NewRequest("POST", "/v1/search", encodeReq(data))
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		respW := httptest.NewRecorder()
 
 		resp, err := s.Server.SearchRequest(respW, req)
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		res := resp.(structs.SearchResponse)
-
-		assert.Equal(1, len(res.Matches))
-		assert.Equal(0, len(res.Matches[structs.Jobs]))
-
-		assert.Equal("0", respW.HeaderMap.Get("X-Nomad-Index"))
+		require.Len(t, res.Matches, 1)
+		require.Len(t, res.Matches[structs.Jobs], 0)
+		require.Equal(t, "0", respW.HeaderMap.Get("X-Nomad-Index"))
 	})
 }
 
-func TestHTTP_Search_AllContext(t *testing.T) {
-	assert := assert.New(t)
+func TestHTTP_PrefixSearch_AllContext(t *testing.T) {
+	t.Parallel()
 
 	testJobID := "aaaaaaaa-e8f7-fd38-c855-ab94ceb89706"
 	testJobPrefix := "aaaaaaaa-e8f7-fd38"
-	t.Parallel()
+
 	httpTest(t, nil, func(s *TestAgent) {
 		createJobForTest(testJobID, s, t)
 
@@ -307,28 +295,24 @@ func TestHTTP_Search_AllContext(t *testing.T) {
 		eval1 := mock.Eval()
 		eval1.ID = testJobID
 		err := state.UpsertEvals(structs.MsgTypeTestSetup, 8000, []*structs.Evaluation{eval1})
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		data := structs.SearchRequest{Prefix: testJobPrefix, Context: structs.All}
 		req, err := http.NewRequest("POST", "/v1/search", encodeReq(data))
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		respW := httptest.NewRecorder()
 
 		resp, err := s.Server.SearchRequest(respW, req)
-		assert.Nil(err)
+		require.NoError(t, err)
 
 		res := resp.(structs.SearchResponse)
-
 		matchedJobs := res.Matches[structs.Jobs]
 		matchedEvals := res.Matches[structs.Evals]
-
-		assert.Equal(1, len(matchedJobs))
-		assert.Equal(1, len(matchedEvals))
-
-		assert.Equal(matchedJobs[0], testJobID)
-		assert.Equal(matchedEvals[0], eval1.ID)
-
-		assert.Equal("8000", respW.HeaderMap.Get("X-Nomad-Index"))
+		require.Len(t, matchedJobs, 1)
+		require.Len(t, matchedEvals, 1)
+		require.Equal(t, testJobID, matchedJobs[0])
+		require.Equal(t, eval1.ID, matchedEvals[0])
+		require.Equal(t, "8000", respW.HeaderMap.Get("X-Nomad-Index"))
 	})
 }
